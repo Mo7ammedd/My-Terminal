fbfeb956c472a5af3ddcc71f3ed56bc4f4a110a5
Mohammed Mostafa <128194288+Mo7ammedd@users.noreply.github.com>
2023-09-02 23:09:42 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -96,7 +96,7 @@ async function getInputValue(){
     createText("<a href='https://www.instagram.com/mohammed__mostaffaa/' target='_blank'><i class='fab fa-instagram white'></i> instagram.com/mohammed__mostaffaa</a>");
     
   }
-  else if(value === "All"){
+  else if(value === "All "){
     trueValue(value);
     createText("Didn't you mean: all")
   }
